78630a35ac3f7dbd0f0ad4ceb79e454a1db0b616
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-04-19 18:49:44 +0700

    generate padding and encrypt plain in /key/publickey

--- routes/key.js
+++ routes/key.js
@@ -1,5 +1,8 @@
 const {Router} = require('express')
 const {Key} = require('../models')
+const crypto = require('crypto')
+const constants = require('constants')
+const fs = require('fs')
 
 const router = Router()
 
@@ -9,12 +12,22 @@ router.post('/publickey', (req,res) => {
 	Key.findOne({publicKey: req.body.publicKey})
 	.then((data) => {
 		if(!data){
+			const padding = crypto.randomBytes(10)
+
 			const newKey = new Key()
 			newKey.publicKey = req.body.publicKey
-			newKey.padding = 'padding'
+			newKey.padding = padding
 
 			newKey.save()
-			.then((data) => res.status(201).send(data))
+			.then((data) => {
+				const plain = fs.readFileSync('./file.txt', 'utf8')
+				let plainBuf = Buffer.from(plain)
+				const padding = Buffer.from(data.padding)
+				plainBuf = Buffer.concat([plainBuf, padding], 256)
+
+				const cipher = crypto.publicEncrypt({"key": data.publicKey, padding: constants.RSA_NO_PADDING}, plainBuf)
+				res.send(cipher)
+			})
 			.catch((error) => console.log(error))
 		}
 		else {
