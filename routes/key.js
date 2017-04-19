e72b04ef4329ef727ef7843cc6930d6032b63ab1
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-04-19 16:46:22 +0700

    can save publickey and padding to db

--- routes/key.js
+++ routes/key.js
@@ -1,11 +1,26 @@
 const {Router} = require('express')
+const {Key} = require('../models')
 
 const router = Router()
 
 router.get('/', (req,res) => res.send('this is key page'))
 
 router.post('/publickey', (req,res) => {
-	res.send(req.body.publicKey)
+	Key.findOne({publicKey: req.body.publicKey})
+	.then((data) => {
+		if(!data){
+			const newKey = new Key()
+			newKey.publicKey = req.body.publicKey
+			newKey.padding = 'padding'
+
+			newKey.save()
+			.then((data) => res.status(201).send(data))
+			.catch((error) => console.log(error))
+		}
+		else {
+			res.status(409).send('this key has been used')
+		}
+	})
 })
 
 module.exports = router
