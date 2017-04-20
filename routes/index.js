4cdc15071e3e6b534eafe8ad807360a3e255d42e
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-04-20 20:07:43 +0700

    /cipher return cipher

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 const {Router} = require('express')
+const {Key, Cipher} = require('../models')
 const key = require('./key')
+const subjects = require('./subjects')
 
 const router = Router()
 
 router.use('/key', key)
+router.use('/subjects', subjects)
 
 router.get('/', (req,res) => res.send('hello, world'))
 
 router.get('/cipher', (req, res) => {
-	res.send({cipher : 'cipher'})
+	Cipher.find()
+	.then((data) => {
+		res.send(data)
+	})
 })
 
 module.exports = router
