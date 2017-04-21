182c3062a34c4525ac9460da7dedc0d23afa656f
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-04-21 20:15:31 +0700

    check state

--- routes/subjects.js
+++ routes/subjects.js
@@ -6,10 +6,12 @@ const router = Router()
 router.get('/', (req,res) => res.send('subjects~~'))
 
 router.get('/:subjectId', (req, res) => {
-	if(req.body.state){
-		res.send('can assess')
+	const isTrue = (req.headers.state == 'true')
+	if(isTrue){
+		res.send({success: true})
 	}
 	else{
+		console.log('state', req.headers.state)
 		axios.get('http://localhost:4000/cipher')
 		.then((response) => {
 			res.send(response.data)
