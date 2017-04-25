6c510a470fdcc340ad4b1c5931627418b7ff0ac2
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-04-25 13:18:01 +0700

    remove route /subjects/:subjectId

--- routes/subjects.js
+++ routes/subjects.js
@@ -5,23 +5,4 @@ const router = Router()
 
 router.get('/', (req,res) => res.send('subjects~~'))
 
-router.get('/:subjectId', (req, res) => {
-	console.log('state', req.headers.state)
-	if(req.headers.state !== undefined){
-		const isTrue = (req.headers.state == 'true')
-		if(isTrue){
-			res.send({success: true})
-		}
-		else{
-			axios.get('http://localhost:4000/cipher')
-			.then((response) => {
-				res.send(response.data)
-			})
-		}
-	}
-	else{
-		res.send('you should auth first')
-	}
-})
-
 module.exports = router
